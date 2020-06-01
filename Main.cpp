9fb3040bcf474d9baee2661a54f127743ff1a5f2
Land-Maze <timurjes@gmail.com>
2020-06-02 02:00:41 +0300

    Start
    Include library

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <stdio.h>
+#include <cstdlib>
 using namespace std;
+bool Menu(){
+    cout << "\n\n\n\
+    \
+    \
+    \
+    \
+    fuck\
+    \
+    \
+    \
+    \
+    "
+}
+
+
+
 
 void main(){
-    cout << "Hello world";
+    Menu();
+
 }
---
+++ README.md
@@ -0,0 +1,4 @@
+# Sea-Battle-cpp
+C++ Game project in Console for UNIX (Posix)
+
+It's project where i posting my work with Sea Battle where i don't use any GUI library only CLI library
